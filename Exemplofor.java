b7f0a5a843db0b775bb5d1aa10bfe6c76f7aabdf
GDB1984 <gdbarros1984@gmail.com>
2024-09-26 11:47:22 -0300

    initial commit

---
+++ Exemplofor.java
@@ -0,0 +1,8 @@
+public class Exemplofor {
+    public static void main(String [] args){
+        for (int cont = 0; cont <= 50; cont++){
+            System.out.println(cont);
+        }
+    }
+    
+}
---
+++ Exemplofor3.java
@@ -0,0 +1,10 @@
+public class Exemplofor3 {
+    public static void main(String[] args){
+
+        int numero;
+        for (numero = 0; numero <10000; numero += 2){
+            System.out.println(" , " + numero + "");
+        }
+    }
+    
+}
