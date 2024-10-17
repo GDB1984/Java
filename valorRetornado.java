44a9775e1afae1a7b6b971f266fb88897eed0324
Guilherme Duarte de Barros <gdbarros1984@gmail.com>
2024-10-17 10:07:55 -0300

    Add files via upload

---
+++ valorRetornado.java
@@ -0,0 +1,14 @@
+public class valorRetornado{
+
+    public static void main(String[] args) {  
+        int valorRetornado;
+        System.out.println("Realizando a soma de dois numeros");
+        valorRetornado = somar(10, 50);  
+        System.out.println("Fim da soma");
+        System.out.println("Soma dos parametros: " + valorRetornado);
+        }
+    
+        public static int somar(int n1, int n2) {  
+            return n1 + n2;
+        }
+    }   
