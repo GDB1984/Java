5d4a5c6cdcca1cf1b4d57654994760160185fd95
kauatwn <kaualopescontato@gmail.com>
2024-11-14 18:23:34 -0300

    exemplo de condicional composta

---
+++ src/conditionals/if_else/examples/compound_conditional/Main.java
@@ -0,0 +1,13 @@
+package conditionals.if_else.examples.compound_conditional;
+
+public class Main {
+    public static void main(String[] args) {
+        int points = 1000;
+
+        if (points >= 1000 && points < 2000) {
+            System.out.println("Você passou para a fase 2!");
+        }
+
+        System.out.println("Próxima instrução!");
+    }
+}
