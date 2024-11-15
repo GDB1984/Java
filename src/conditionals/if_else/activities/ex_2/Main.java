2d4c28a19674b9a482350890807c6eccdbd6aac2
kauatwn <kaualopescontato@gmail.com>
2024-11-14 21:08:12 -0300

    Exercício 2

---
+++ src/conditionals/if_else/activities/ex_2/Main.java
@@ -0,0 +1,25 @@
+package conditionals.if_else.activities.ex_2;
+
+/* Faça um algoritmo que:
+    - Receba a idade de uma pessoa;
+    - Se for maior de idade imprima: "maior de idade";
+    - Senão imprima: "menor de idade". */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a idade da pessoa: ");
+        int age = sc.nextInt();
+
+        if (age >= 18) {
+            System.out.println("Maior de idade");
+        } else {
+            System.out.println("Menor de idade");
+        }
+
+        sc.close();
+    }
+}
