34c2e6a690ed81c09785e499c9585ce39717cdfb
kauatwn <kaualopescontato@gmail.com>
2024-11-15 12:16:20 -0300

    Loops - Exercício 1

---
+++ src/loops/activities/ex_1/Main.java
@@ -0,0 +1,14 @@
+package loops.activities.ex_1;
+
+// Escreva um algoritmo que imprima a sequência do número 100 ao 200 usando while.
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 100;
+
+        while (number <= 200){
+            System.out.println(number);
+            number++;
+        }
+    }
+}
