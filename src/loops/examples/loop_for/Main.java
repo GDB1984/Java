6a4059d284fc762fc4bbde1b937c81ea76ab948f
kauatwn <kaualopescontato@gmail.com>
2024-11-15 12:56:18 -0300

    Exemplo loop for

---
+++ src/loops/examples/loop_for/Main.java
@@ -0,0 +1,9 @@
+package loops.examples.loop_for;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int count = 0; count <= 50; count++) {
+            System.out.println(count);
+        }
+    }
+}
