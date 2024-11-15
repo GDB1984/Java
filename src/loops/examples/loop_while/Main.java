b411215fb41ac81a86ff8165af852c9024cfadf6
kauatwn <kaualopescontato@gmail.com>
2024-11-15 11:59:26 -0300

    Exemplo loop while

---
+++ src/loops/examples/loop_while/Main.java
@@ -0,0 +1,12 @@
+package loops.examples.loop_while;
+
+public class Main {
+    public static void main(String[] args) {
+        int count = 1;
+
+        while (count <= 100){
+            System.out.println(count);
+            count++;
+        }
+    }
+}
